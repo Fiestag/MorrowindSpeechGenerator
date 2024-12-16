52b91fcfd5f8bc98b000aff6e6c17d39e0fc87df
Fiestag <149173284+Fiestag@users.noreply.github.com>
2024-12-16 17:56:43 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,13 @@
 @echo off
 REM Télécharge et installe Python 3.10
+
+curl -L -o tes3conv.zip https://github.com/Greatness7/tes3conv/releases/download/v0.3.0/windows-latest.zip
+powershell -Command "Expand-Archive -Path 'tes3conv.zip'"
+xcopy "%cd%\tes3conv" "%cd%" /E /I /Y
+del tes3conv.zip
+rmdir /S /Q tes3conv
+
+
 echo Download Python 3.10...
 curl -o python-3.10.0.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
 
@@ -17,5 +25,5 @@ echo Install Dependencies
 powershell -Command "Start-Process 'python' -ArgumentList 'setup.py' -Verb RunAs -Wait"
 
 echo Installation Succesful !
-UI.py
+python UI.py
 pause
