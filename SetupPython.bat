fbc2601fb9aa1889b02d64a39d90da604abedb20
Fiestag <149173284+Fiestag@users.noreply.github.com>
2024-12-17 09:51:11 +0100

    Update SetupPython.bat

--- SetupPython.bat
+++ SetupPython.bat
@@ -1,20 +1,12 @@
 @echo off
 
-echo Downloading Python 3.10...
-curl -L -o python-3.10.0.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
+echo Download Python 3.10...
+curl -o python-3.10.0.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
 
-echo Please select the installation directory for Python 3.10...
-powershell -Command "Add-Type -AssemblyName System.Windows.Forms; $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog; $result = $folderBrowser.ShowDialog(); if ($result -eq 'OK') {$folderBrowser.SelectedPath} else {exit 1}" > selected_path.txt
-set /p TargetDir=<selected_path.txt
-if "%TargetDir%"=="" (
-    echo No directory selected. Exiting.
-    exit /b 1
-)
+echo Install Python 3.10...
+start /wait python-3.10.0.exe 
 
-echo Installing Python 3.10 in %TargetDir%...
-start /wait python-3.10.0.exe /quiet InstallAllUsers=0 PrependPath=1 TargetDir="%TargetDir%"
-
-echo Restart the computer to complete Python installation? (O/N)
+echo Restart Computer for complete Python Installation ? (O/N)
 set /p choice=
 if /i "%choice%"=="O" (
     shutdown /r /t 0
@@ -23,4 +15,3 @@ if /i "%choice%"=="O" (
 )
 
 del python-3.10.0.exe
-del selected_path.txt
